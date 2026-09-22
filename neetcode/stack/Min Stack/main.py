5ec9e5b4046dedf7cfbf07fe26a5532da8fcc8c5
lnevoss <forgjj@proton.me>
2026-09-22 22:48:00 +0200

    completed min_stack task

--- neetcode/stack/Min Stack/main.py
+++ neetcode/stack/Min Stack/main.py
@@ -1,16 +1,25 @@
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
+        self.min_stack = [2**31 - 1]        
+
 
     def push(self, val: int) -> None:
-        
+        if val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        self.stack.append(val)
 
     def pop(self) -> None:
-        
+        if self.stack:
+            if self.stack.pop() == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self) -> int:
-        
+        if self.stack:
+            return self.stack[-1]
 
     def getMin(self) -> int:
-        
+        if self.stack:
+            return self.min_stack[-1]
+
